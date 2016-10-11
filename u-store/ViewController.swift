c32954bbb0296cf3bdbdaca82f873935b2cc7ceb
tran.quoc.quan <tran.quoc.quan@softfront.com.vn>
2016-10-11 10:39:06 +0900

    Initial Commit

---
+++ u-store/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  u-store
+//
+//  Created by tran.quoc.quan on 10/11/16.
+//  Copyright © 2016 tran.quoc.quan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
